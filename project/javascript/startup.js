60e41fef96d364c4983f47bceb850ed312870b07
Adnan Muzaferovic <a.muzaferovic@students.htl-leonding.ac.at>
2026-04-07 15:03:29 +0200

    Add init() function to startup.js; link js to html

--- project/javascript/startup.js
+++ project/javascript/startup.js
@@ -1,1 +1,13 @@
 let body = document.getElementsByTagName('body')[0]
+
+function init() {
+    body.innerHTML = `
+    <div id="textContainer">
+    <h3>EPILEPSY WARNING: Flashing lights and loud noises may occur. 
+    Consider talking to a medical professional before playing.</h3>
+    <h3>Fullscreen (F11) and headphones are advised for best experience.</h3>
+    <div>`
+
+}
+
+init();
