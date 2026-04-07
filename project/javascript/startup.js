aa25ded86d46f210faf3631a8effae27828d0fce
Adnan Muzaferovic <a.muzaferovic@students.htl-leonding.ac.at>
2026-04-07 16:22:49 +0200

    Add font and fade in/out animation

--- project/javascript/startup.js
+++ project/javascript/startup.js
@@ -6,8 +6,18 @@ function init() {
     <h3>EPILEPSY WARNING: Flashing lights and loud noises may occur. 
     Consider talking to a medical professional before playing.</h3>
     <h3>Fullscreen (F11) and headphones are advised for best experience.</h3>
-    <div>`
-
+    </div>`
+    let textContainer = document.getElementById('textContainer');
+    textContainer.style.animation = 'fadeOut 2s 3s ease-in-out'
+    setTimeout(function() {
+        textContainer.style.opacity = 0
+        textContainer.innerHTML = `
+        <h1 id="title">GameCorner</h1>`
+    }, 5000)
+    textContainer.style.animation = 'fadeIn 2s 6s ease-in-out'
+    setTimeout(function() {
+        textContainer.style.opacity = 1
+    }, 8000)
 }
 
 init();
